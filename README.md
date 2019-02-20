3a3840afbd50b89bce4f7e0d4e0b9fb976da91d7
Bryce <wlhsfrc@gmail.com>
2019-02-19 19:01:53 -0800

    a test commit by bryce!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FRC-2019
+
+How to install CScore:
+-
+- 
--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -20,7 +20,7 @@ public final class Main {
 
   /**
    * Main initialization function. Do not perform any initialization here.
-   *
+   * test comment v1
    * <p>If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
